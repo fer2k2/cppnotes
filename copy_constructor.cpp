d3d17bc42a1536b93262f36232e1966cbfd5131e
fer2k2 <fer2k2@users.noreply.github.com>
2019-03-03 19:02:45 -0600

    Update copy_constructor.cpp

--- copy_constructor.cpp
+++ copy_constructor.cpp
@@ -6,7 +6,26 @@ class IntArray {
 };
 
 // Default memer-wise copy constructor
+// a and b point to the same location (shalow copy)
+// modifications to a are reflected to b and viceversa
+IntArray b = a;
+b.m_ptr = a.m_ptr;
+b.m_size = a.m_size;
 
+// When any of the objects is out of scope, it calles the corresponding destructor
+delete[] a.m_ptr
+// Causing a Double-delete bug when the oher's object destructor is called
+delete[] b.m_ptr // Runtime error: memory was already freeed
+
+// A basic solution would be to delete the default copy constructor
+
+class IntArray {
+  public:
+    // Copy constructor
+    IntArray(const IntArray&) = delete;
+    ...
+};
 
 // Simple copy constructor
+
 
