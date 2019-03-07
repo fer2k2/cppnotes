f56d36f4f0eb38b2a4e7bec1eb72972908ab71e9
fer2k2 <fer2k2@users.noreply.github.com>
2019-03-06 23:05:41 -0600

    Update copy_constructor.cpp

--- copy_constructor.cpp
+++ copy_constructor.cpp
@@ -18,7 +18,6 @@ delete[] a.m_ptr
 delete[] b.m_ptr // Runtime error: memory was already freeed
 
 // A basic solution would be to delete the default copy constructor
-
 class IntArray {
   public:
     // Copy constructor
@@ -26,6 +25,18 @@ class IntArray {
     ...
 };
 
-// Simple copy constructor
+// A better ide ais to implement our own copy constructor that performs Deep-copying
+IntArray(const IntArray& source)
+{
+  if (!source.IsEmpty()) {
+    m_size = source.m_size;
+    m_ptr = new int[m_size]{};
+    
+    for (int i=0; i < m_size; i++) {
+      m_ptr[i] = source.m_ptr[i];  
+    }
+  }
+}
+
 
 
